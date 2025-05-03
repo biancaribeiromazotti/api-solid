fc36c120ab6e7eeedb19784dd5ec9b7af279fd34
Bianca Ribeiro Mazotti <biancaribeiromazotti@gmail.com>
2025-05-03 13:02:56 -0300

    feat(user):Criei a rota de criação de usuário.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify';
-import { PrismaClient } from '../generated/prisma'
+import { appRoutes } from './http/routes';
+
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.register(appRoutes)
